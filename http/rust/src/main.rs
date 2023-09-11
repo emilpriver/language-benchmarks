4fce081bc4ba573f10ef44c9765058785a756e46
Emil Privér <emil@priver.dev>
2023-09-11 15:36:14 +0200

    updated port

--- http/rust/src/main.rs
+++ http/rust/src/main.rs
@@ -8,7 +8,7 @@ async fn root() -> impl Responder {
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(root))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", 3000))?
         .run()
         .await
 }
