a9ec925e14ccc4572ae554e73f33b226831a2056
Emil Privér <emil@priver.dev>
2023-10-08 21:16:07 +0200

    Worke don adding concurrency to ocaml project

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -4,11 +4,13 @@ type response = {
 [@@deriving yojson]
 
 let () =
-  Dream.run
-  @@ Dream.logger
-  @@ Dream.router [
-    Dream.get "/" (fun _ ->
-      let json = response_to_yojson {message = "Hello from Ocaml" } 
-      |> Yojson.Safe.to_string in
-      Dream.json json);
-  ]
+  Eio_main.run (fun _ ->
+    Dream.run
+    @@ Dream.logger
+    @@ Dream.router [
+      Dream.get "/" (fun _ ->
+        let json = response_to_yojson {message = "Hello from Ocaml" } 
+        |> Yojson.Safe.to_string in
+        Dream.json json);
+    ]
+  )
