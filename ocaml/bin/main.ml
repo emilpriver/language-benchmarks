2f310bba5420141ba93daf7d748adbe94baef807
Emil Privér <emil@priver.dev>
2023-10-08 20:04:57 +0200

    ocaml

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,12 +1,14 @@
-let json_string = {|
-  {"message" : "Hello from OCaml"}|}
+type response = {
+  message: string;
+}
+[@@deriving yojson]
 
 let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
     Dream.get "/" (fun _ ->
-      let json = Yojson.Safe.from_string json_string in
-      let html_string = Yojson.Safe.to_string json in
-      Dream.html html_string);
+      let json = response_to_yojson {message = "Hello from Ocaml" } 
+      |> Yojson.Safe.to_string in
+      Dream.json json);
   ]
